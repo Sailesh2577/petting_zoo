924bac2e62f7b9a9a129a37a5047dec298e266ac
Sailesh Pujara <spujara@huskers.unl.edu>
2020-04-10 08:41:58 -0500

    Added moving functionality

--- src/main/java/edu/unl/cse/csce361/petting_zoo/controller/animalHandler/moveButtons/MoveAnimalsToPettingEnclosureButton.java
+++ src/main/java/edu/unl/cse/csce361/petting_zoo/controller/animalHandler/moveButtons/MoveAnimalsToPettingEnclosureButton.java
@@ -3,6 +3,7 @@ package edu.unl.cse.csce361.petting_zoo.controller.animalHandler.moveButtons;
 import edu.unl.cse.csce361.petting_zoo.controller.Command;
 import edu.unl.cse.csce361.petting_zoo.model.AnimalEntity;
 import edu.unl.cse.csce361.petting_zoo.model.HibernateUtil;
+import edu.unl.cse.csce361.petting_zoo.model.PettingZoo;
 import edu.unl.cse.csce361.petting_zoo.view.UserInterfaceManager;
 import org.hibernate.query.Query;
 
@@ -16,27 +17,23 @@ public class MoveAnimalsToPettingEnclosureButton implements Command {
 
     @Override
     public void execute() {
+        HibernateUtil.getSession().beginTransaction();
         Query query = HibernateUtil.getSession().createQuery("from AnimalEntity");
         StringBuilder outputStr = new StringBuilder();
-        int count = 0;
-        outputStr.append(toString());
-        outputStr.append(": \n");
+
+        outputStr.append("Moving the animals: \n");
         for (Object result : query.getResultList()) {
             AnimalEntity animalEntityObject = (AnimalEntity) result;
-            if(animalEntityObject.getLocation().getName().equalsIgnoreCase("Petting Enclosure 1")) {
-                count++;
-                outputStr.append(count);
-                outputStr.append(") ");
-                outputStr.append(animalEntityObject.getName());
-                outputStr.append(" of type: ");
-                outputStr.append(animalEntityObject.getType());
-                outputStr.append("\n");
-            }
+            outputStr.append("Moving ");
+            outputStr.append(animalEntityObject.getName());
+            outputStr.append("\n");
         }
-        outputStr.append("\n");
-        outputStr.append("Number of Animals at Petting Enclosure 1: ");
-        outputStr.append(count);
+        outputStr.append("Moved all the animals to Petting Enclosure 1!");
         UserInterfaceManager.getUI().showInformation(outputStr.toString());
+        javax.persistence.Query query1 = HibernateUtil.getSession().createQuery("UPDATE AnimalEntity SET location_name = 'Petting Enclosure 1'");
+        query1.executeUpdate();
+        HibernateUtil.getSession().save(PettingZoo.getPettingZoo());
+        HibernateUtil.getSession().getTransaction().commit();
     }
 }
 
--- src/main/java/edu/unl/cse/csce361/petting_zoo/controller/animalHandler/moveButtons/MoveAnimalsToTheBarnButton.java
+++ src/main/java/edu/unl/cse/csce361/petting_zoo/controller/animalHandler/moveButtons/MoveAnimalsToTheBarnButton.java
@@ -1,6 +1,11 @@
 package edu.unl.cse.csce361.petting_zoo.controller.animalHandler.moveButtons;
 
 import edu.unl.cse.csce361.petting_zoo.controller.Command;
+import edu.unl.cse.csce361.petting_zoo.model.AnimalEntity;
+import edu.unl.cse.csce361.petting_zoo.model.HibernateUtil;
+import edu.unl.cse.csce361.petting_zoo.model.PettingZoo;
+import edu.unl.cse.csce361.petting_zoo.view.UserInterfaceManager;
+import org.hibernate.query.Query;
 
 public class MoveAnimalsToTheBarnButton implements Command {
 
@@ -9,4 +14,27 @@ public class MoveAnimalsToTheBarnButton implements Command {
     public String toString() {
         return "Move animals to the Barn";
     }
+
+    @Override
+    public void execute() {
+        HibernateUtil.getSession().beginTransaction();
+        Query query = HibernateUtil.getSession().createQuery("from AnimalEntity");
+        StringBuilder outputStr = new StringBuilder();
+
+        outputStr.append("Moving the animals: \n");
+        for (Object result : query.getResultList()) {
+            AnimalEntity animalEntityObject = (AnimalEntity) result;
+            outputStr.append("Moving ");
+            outputStr.append(animalEntityObject.getName());
+            outputStr.append("\n");
+        }
+        outputStr.append("Moved all the animals to The Barn!");
+        UserInterfaceManager.getUI().showInformation(outputStr.toString());
+        javax.persistence.Query query1 = HibernateUtil.getSession().createQuery("UPDATE AnimalEntity SET location_name = 'The Barn'");
+        query1.executeUpdate();
+        HibernateUtil.getSession().save(PettingZoo.getPettingZoo());
+        HibernateUtil.getSession().getTransaction().commit();
+    }
 }
+
+
